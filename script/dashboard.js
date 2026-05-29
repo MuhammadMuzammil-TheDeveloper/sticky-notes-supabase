40232b9bfaa8eb577746437658fa7dffc2c555b6
MuhammadMuzammil-TheDeveloper <muzammil.thedeveloper@gmail.com>
2026-05-29 07:04:53 +0500

    Performing CRUD

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,8 +1,58 @@
-function createNote(){
-    const title = document.getElementById(`title`).value;
-    const content = document.getElementById(`content`).value;
-    let notes = "
-    "
-    console.log(title, content)
+function createNote() {
+  const titleInput = document.getElementById("title");
+  const contentInput = document.getElementById("content");
+
+  const title = titleInput.value;
+  const content = contentInput.value;
+
+  let notesContainer = document.getElementById("notes-container");
+
+  // Validation
+  if (title === "" || content === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  console.log(title, content);
+
+  let notes = `
+    <div class="note">
+
+      <button class="delete-btn" onClick="deleteEach(this)">X</button>
+      <button class="edit-btn" onClick="editEach(this)">Edit</button>
+
+      <h3>${title}</h3>
+      <p>${content}</p>
+
+    </div>
+  `;
+
+  notesContainer.innerHTML += notes;
+
+  console.log("Notes Container", notesContainer);
+
+  // Clear Inputs
+  titleInput.value = "";
+  contentInput.value = "";
+}
+
+function deleteEach(button) {
+  console.log("workingggg//....");
+  console.log(button);
+  button.parentElement.remove();
+}
+function editEach(button) {
+  const note = button.parentElement;
+  const title = note.querySelector("h3");
+  const content = note.querySelector("p");
+  const newTitle = prompt(`Edit Text`, title.innerText);
+  const newContent = prompt(`Edit Text`, content.innerText);
+  title.innerText = newTitle;
+  content.innerText = newContent;
+}
+
+function deleteAllNote(){
+    let notesContainer = document.getElementById("notes-container");
+    notesContainer.innerHTML="";
 
 }
